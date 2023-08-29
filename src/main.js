2e18031c8b1d811102a855741457753d917ed730
Nick <icguanyu@gmail.com>
2023-08-29 23:32:54 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,11 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@ungap/custom-elements';
+import vueCustomElement from 'vue-custom-element'
+Vue.use(vueCustomElement)
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+App.store = store
+App.router = router
+Vue.customElement('asus-widget', App)
+
+// new Vue({
+//   router,
+//   store,
+//   render: h => h(App)
+// }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
+const webpack = require("webpack");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 1,
+      }),
+    ],
+  },
+  chainWebpack: (config) => {
+    config.optimization.delete("splitChunks");
+  },
+  filenameHashing: false,
+  pages: {
+    index: {
+      entry: "src/main.js",
+      template: "public/index.html",
+      filename: "index.html",
+      title: "Asus-Chat-Widget",
+    },
+  },
+});
